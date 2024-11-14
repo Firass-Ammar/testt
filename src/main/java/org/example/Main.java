1e5296ecc2ac83638d86a6d38be101142a317dc5
Firass-Ammar <firasam3@gmail.com>
2024-11-14 14:27:21 +0100

    kk

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,8 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
             System.out.println("jj = " + i);
-
+            System.out.println("jj = " + i);
+            System.out.println("jj = " + i);
         }
     }
 }
